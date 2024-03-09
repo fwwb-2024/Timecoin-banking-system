143ec77a286e0bb27a2a1fc41595347c23e27b2e
makabakax <2583221689@qq.com>
2024-03-09 16:57:26 +0800

    添加测试环境

---
+++ timesystem/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    testTimeout: 10000,
+    reporters: [
+        'default'
+    ],
+    watchPathIgnorePatterns: ['/node_modules/', '/dist/', '/.git/'],
+    moduleFileExtensions: ['js', 'json'],
+    rootDir: __dirname,
+    testMatch: ["<rootDir>/pages/index/index.test.js"],
+    testPathIgnorePatterns: ['/node_modules/']
+}
